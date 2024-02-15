d32d8b7d9cd178be1179cad9c7a483dfc99e1f7f
caidam <constant.aidam@proton.me>
2024-02-15 10:17:46 +0100

    corrected base name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/caidam/inside-abnb-front",
+  base: "/inside-abnb-front",
   plugins: [react()],
 })
